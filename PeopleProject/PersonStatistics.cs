a8753a86577023362228641be50c65d061ee234d
Domigy <domi.adamerik@gmail.com>
2024-05-31 19:30:56 +0200

    2. feladat

---
+++ PeopleProject/PersonStatistics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleProject
+{
+    public class PersonStatistics
+    {
+        private List<Person> persons;
+
+        public PersonStatistics(List<Person> persons)
+        {
+            this.persons = persons;
+        }
+
+        internal List<Person> Persons { get => persons; private set => persons = value; }
+    }
+}
