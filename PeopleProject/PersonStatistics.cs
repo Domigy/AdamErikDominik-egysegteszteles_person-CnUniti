3d43dcdb1baf690fc1f738eb3077d43ac463124f
Domigy <domi.adamerik@gmail.com>
2024-05-31 20:15:36 +0200

    3. feladat, Tesztek bekerültek

--- PeopleProject/PersonStatistics.cs
+++ PeopleProject/PersonStatistics.cs
@@ -8,13 +8,45 @@ namespace PeopleProject
 {
     public class PersonStatistics
     {
-        private List<Person> persons;
+        public List<Person> persons;
 
         public PersonStatistics(List<Person> persons)
         {
             this.persons = persons;
+            Persons = persons ?? throw new ArgumentNullException(nameof(persons));
         }
+   
 
         internal List<Person> Persons { get => persons; private set => persons = value; }
+        public double GetAverageAge()
+        {
+            return Persons.Average(p => p.Age);
+        }
+
+        public int GetNumberOfStudents()
+        {
+            return Persons.Count(p => p.IsStudent);
+        }
+
+        public Person GetPersonWithHighestScore()
+        {
+            return Persons.OrderByDescending(p => p.Score).FirstOrDefault();
+        }
+
+        public double GetAverageScoreOfStudents()
+        {
+            var students = Persons.Where(p => p.IsStudent);
+            return students.Any() ? students.Average(p => p.Score) : 0;
+        }
+
+        public Person GetOldestStudent()
+        {
+            return Persons.Where(p => p.IsStudent).OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+
+        public bool IsAnyoneFailing()
+        {
+            return Persons.Any(p => p.Score < 40);
+        }
     }
 }
--- TestPeopleProject/UnitTest1.cs
+++ TestPeopleProject/UnitTest1.cs
@@ -1,3 +1,4 @@
+using TestPeopleProject;
 namespace TestPeopleProject
 {
     public class Tests
@@ -12,5 +13,104 @@ namespace TestPeopleProject
         {
             Assert.Pass();
         }
+    }
+    [TestFixture]
+    public class PersonStatisticsTests
+    {
+        [Test]
+        public void TestGetAverageAge()
+        {
+            List<Person> people = new List<Person>();
+            Person ember1 = new Person(1, "Tamás", 20, true, 12);
+            Person ember2 = new Person(2, "Dóra", 30, false, 43);
+            Person ember3 = new Person(3, "Panna", 40, true, 76);
+            people.Add(ember1);
+            people.Add(ember2);
+            people.Add(ember3);
+
+            var stats = new PersonStatistics(people);
+
+            Assert.AreEqual(30, stats.GetAverageAge());
+        }
+        [Test]
+        public void TestGetNumberOfStudents()
+        {
+            List<Person> people = new List<Person>();
+            Person ember1 = new Person(1, "Tamás", 20, true, 12);
+            Person ember2 = new Person(2, "Dóra", 30, false, 43);
+            Person ember3 = new Person(3, "Panna", 40, true, 76);
+            people.Add(ember1);
+            people.Add(ember2);
+            people.Add(ember3);
+
+            var stats = new PersonStatistics(people);
+
+            Assert.AreEqual(2, stats.GetNumberOfStudents());
+        }
+
+        [Test]
+        public void TestGetPersonWithHighestScore()
+        {
+            List<Person> people = new List<Person>();
+            Person ember1 = new Person(1, "Tamás", 20, true, 12);
+            Person ember2 = new Person(2, "Dóra", 30, false, 43);
+            Person ember3 = new Person(3, "Panna", 40, true, 76);
+            people.Add(ember1);
+            people.Add(ember2);
+            people.Add(ember3);
+
+            var stats = new PersonStatistics(people);
+
+            Assert.AreEqual(76, stats.GetPersonWithHighestScore().Score);
+        }
+
+        [Test]
+        public void TestGetAverageScoreOfStudents()
+        {
+            List<Person> people = new List<Person>();
+            Person ember1 = new Person(1, "Tamás", 20, true, 80);
+            Person ember2 = new Person(2, "Dóra", 30, false, 90);
+            Person ember3 = new Person(3, "Panna", 40, true, 70);
+            people.Add(ember1);
+            people.Add(ember2);
+            people.Add(ember3);
+
+            var stats = new PersonStatistics(people);
+
+            Assert.AreEqual(75, stats.GetAverageScoreOfStudents());
+        }
+
+        [Test]
+        public void TestGetOldestStudent()
+        {
+            List<Person> people = new List<Person>();
+            Person ember1 = new Person(1, "Tamás", 20, true, 12);
+            Person ember2 = new Person(2, "Dóra", 30, false, 43);
+            Person ember3 = new Person(3, "Panna", 40, true, 95);
+            people.Add(ember1);
+            people.Add(ember2);
+            people.Add(ember3);
+
+            var stats = new PersonStatistics(people);
+
+            Assert.AreEqual(40, stats.GetOldestStudent().Age);
+        }
+
+        [Test]
+        public void TestIsAnyoneFailing()
+        {
+            List<Person> people = new List<Person>();
+            Person ember1 = new Person(1, "Tamás", 20, true, 12);
+            Person ember2 = new Person(2, "Dóra", 30, false, 43);
+            Person ember3 = new Person(3, "Panna", 40, true, 76);
+            people.Add(ember1);
+            people.Add(ember2);
+            people.Add(ember3);
+
+            var stats = new PersonStatistics(people);
+
+            Assert.IsTrue(stats.IsAnyoneFailing());
+        }
+
     }
 }
